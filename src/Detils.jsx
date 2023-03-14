77047f65f6f4b8d28bfc778c4e6391573a74c90d
chrisIyer <hellohotbox420@gmail.com>
2023-03-14 13:08:21 -0400

    added App.js and a couple components.

---
+++ src/Detils.jsx
@@ -0,0 +1,12 @@
+import { useNavigate, useParams } from 'react-router-dom'
+import { useQuery } from "@tanstack/react-query"
+import { useContext, useState } from 'react'
+import AdoptedPetContext from './AdoptedPetContext.jsx'
+import Modal from './Modal'
+import ErrorBoundary from './ErrorBoundary'
+import fetchPet from './fetchPet'
+import Carousel from './Carousel'
+
+const Details = () => {
+     const {}
+}
